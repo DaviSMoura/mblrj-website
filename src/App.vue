ef2c3ad0b77557d0e884c0a839fb1993724c3777
Davi Moura <davisilvag4mer@gmail.com>
2022-11-20 17:36:15 -0300

    feat: footer

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import Navbar from './components/Navbar.vue';
+import FooterComponent from './components/Footer.vue';
 </script>
 
 <template>
   <navbar></navbar>
   <router-view />
+  <footer-component></footer-component>
 </template>
---
+++ src/components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex flex-col justify-center py-3 bg-black text-white text-center">
+    <div>© 2022 | MBL RJ</div>
+  </div>
+</template>
