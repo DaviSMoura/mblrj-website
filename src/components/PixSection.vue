179adfa7ae80d60ef4a55342294fbea719bf00b8
Davi Moura <davisilvag4mer@gmail.com>
2022-11-20 17:36:09 -0300

    feat: better pix section

--- src/components/PixSection.vue
+++ src/components/PixSection.vue
@@ -1,16 +1,22 @@
 <template>
-  <div class="xl:container mx-8">
+  <!-- <div class="xl:container mx-8">
     <h1 class="text-primary text-4xl font-bold text-center my-8">
-      Contribua no PIX
+      Doação livre
     </h1>
-  </div>
-  <div class="flex flex-col justify-center">
-    <img
-      src="../assets/pix.png"
-      alt="pix"
-      width="auto"
-      height="auto"
-      class="object-scale-down w-auto h-auto max-w-fit max-h-96"
-    />
+  </div> -->
+  <div class="flex justify-center my-5">
+    <div class="flex flex-col place-items-center text-center">
+      <b>
+        Você também pode fazer uma doação livre sem se tornar um contribuinte mensal.
+      </b>
+      <img
+        src="../assets/pix.png"
+        alt="QR code do pix"
+        width="auto"
+        height="auto"
+        class="object-scale-down w-auto h-auto max-w-fit max-h-96"
+      />
+      <p>Nossa chave PIX: <b>contribua@mblrj.com</b></p>
+    </div>
   </div>
 </template>
