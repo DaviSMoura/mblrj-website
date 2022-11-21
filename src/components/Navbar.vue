e4a226626acc5fa2a96efb6277ac78626c85a766
Davi Moura <davisilvag4mer@gmail.com>
2022-11-21 20:49:28 -0300

    feat: remove dropdown from navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -12,14 +12,14 @@ const navigationLinks = ref([
 <template>
 <div class="navbar bg-base-100 sticky top-0 z-50 bg-primary text-primary-content">
   <div class="navbar-start">
-    <div class="dropdown">
+    <!-- <div class="dropdown">
       <label tabindex="0" class="btn btn-ghost btn-circle">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
       </label>
       <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
         <li><router-link :to="link.target" v-for="link of navigationLinks" :key="link.target">{{ link.text }}</router-link></li>
       </ul>
-    </div>
+    </div> -->
   </div>
   <div class="navbar-center">
     <router-link class="btn btn-ghost normal-case text-xl" to="/">
