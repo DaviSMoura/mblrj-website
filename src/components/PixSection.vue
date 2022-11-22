4ec4c15e49001983e86f22acade1bed21ccafb76
Davi Moura <davisilvag4mer@gmail.com>
2022-11-21 22:27:51 -0300

    feat: scale down pix

--- src/components/PixSection.vue
+++ src/components/PixSection.vue
@@ -4,7 +4,7 @@
       Doação livre
     </h1>
   </div> -->
-  <div class="flex justify-center my-5">
+  <div class="flex justify-center py-8">
     <div class="flex flex-col place-items-center text-center">
       <b>
         Você também pode fazer uma doação livre sem se tornar um contribuinte mensal.
@@ -14,7 +14,7 @@
         alt="QR code do pix"
         width="auto"
         height="auto"
-        class="object-scale-down w-auto h-auto max-w-fit max-h-96"
+        class="object-scale-down w-auto max-w-fit h-[70%]"
       />
       <p>Nossa chave PIX: <b>contribua@mblrj.com</b></p>
     </div>
