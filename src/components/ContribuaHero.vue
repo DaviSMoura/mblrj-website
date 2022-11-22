33939f903f49e0624c1c9eb2861dad9bb4ed0906
Davi Moura <davisilvag4mer@gmail.com>
2022-11-21 22:51:30 -0300

    feat: scroll to

--- src/components/ContribuaHero.vue
+++ src/components/ContribuaHero.vue
@@ -8,7 +8,7 @@
     <div class="max-w-lg">
       <h1 class="text-5xl font-bold">O RJ precisa de você!</h1>
       <p class="py-6">Não recebemos dinheiro público e precisamos da sua ajuda para continuarmos atuando no nosso estado. Precisamos replicar o trabalho do MBL Nacional no nosso estado.</p>
-      <button class="btn btn-primary">Contribuir Mensalmente</button>
+      <a href="#contribuir" class="btn btn-primary">Contribuir Mensalmente</a>
     </div>
   </div>
 </div>
