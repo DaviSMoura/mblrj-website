a3aa50e5f88cabaaeb9db6e44b53a020aaf8156d
Davi Moura <davisilvag4mer@gmail.com>
2022-11-24 09:06:58 -0300

    fix: typo

--- src/components/ContribuaHero.vue
+++ src/components/ContribuaHero.vue
@@ -7,7 +7,7 @@
   <div class="hero-content text-center text-neutral-content">
     <div class="max-w-lg">
       <h1 class="text-5xl font-bold">Nosso estado pede socorro!</h1>
-      <p class="py-6">Ajude o MBL RJ nesta luta! Não recebemos dinheiro público e contamos com a sua contribuição para replicar o trabalho do nacional do MBL no nosso estado!</p>
+      <p class="py-6">Ajude o MBL RJ nesta luta! Não recebemos dinheiro público e contamos com a sua contribuição para replicar o trabalho nacional do MBL no nosso estado!</p>
       <a href="#contribuir" class="btn btn-primary">Contribuir Mensalmente</a>
     </div>
   </div>
