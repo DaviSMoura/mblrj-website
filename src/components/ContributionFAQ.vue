42f7b93d602979c67ec053f409b693db984fc697
Davi Moura <davisilvag4mer@gmail.com>
2022-11-23 22:37:26 -0300

    feat: new qa

--- src/components/ContributionFAQ.vue
+++ src/components/ContributionFAQ.vue
@@ -8,24 +8,44 @@ const questions = ref([
     a: "Fundado em 2014, o Movimento Brasil Livre (MBL) é um movimento político identificado por especialistas como liberal conservador.",
   },
   {
-    q: "Como faço para contribuir com cartão de crédito?",
-    a: "Contribuição via cartão de crédito é feita através do Stripe, uma plataforma de pagamentos segura e confiável. Para contribuir, basta clicar no botão 'Contribuir Mensalmente' na página de contribuição e selecionar um plano. O Stripe irá te redirecionar para uma página de pagamento, onde você poderá inserir os dados do seu cartão de crédito. Após a confirmação do pagamento, você será redirecionado para a página de contribuição, onde poderá ver o seu recibo de contribuição. Após a contribuição, nós entraremos em contato via Whatsapp para te dar as boas vindas e te redirecionar pro nosso grupo exclusivo.",
+    q: "Qual o valor da mensalidade? Posso contribuir com mais?",
+    a: "A mensalidade voluntária do MBL RJ é de R$ 20 por mês. Qualquer contribuição a mais é opcional (assim como o valor de R$ 20,00), mas muito bem-vinda.",
   },
   {
-    q: "Como faço para contribuir com PIX?",
-    a: "Você sempre pode fazer uma contribuição avulsa através do nosso PIX: contribua@mblrj.com",
+    q: "O que eu ganho sendo mensalista do MBL RJ?",
+    a: "Além de colaborar com o movimento, você ainda fará parte de um grupo exclusivo no qual poderá participar ativamente das nossas discussões e decisões.",
   },
   {
-    q: "Qual é o valor mínimo para contribuir?",
-    a: "Você pode contribuir de forma avulsa com qualquer valor. Ou contribuir com um dos valores dos planos acima mensalmente de forma automática no seu cartão de crédito.",
+    q: "Qual a forma de pagamento?",
+    a: "Você pode pagar via cartão de crédito por meio da plataforma Stripe. Basta escolher seu plano e inserir os dados do seu cartão. Assim que o pagamento for confirmado, você será redirecionado ao grupo exclusivo.",
   },
   {
-    q: "Posso cancelar os pagamentos recorrentes?",
-    a: "Entre em contato com a gente via Whatsapp ou Redes sociais e nós cancelaremos o seu pagamento.",
+    q: "Posso fazer uma doação avulsa sem me comprometer mensalmente?",
+    a: "Com certeza! Você pode contribuir pelo PIX, a chave é contribua@mblrj.com, mas você não terá acesso aos benefícios dos mensalistas.",
   },
   {
-    q: "Como entrar no grupo exclusivo dos mensalistas?",
-    a: "Entraremos em contato via Whatsapp para te dar as boas vindas e te redirecionar pro nosso grupo exclusivo.",
+    q: "Como eu sei se o meu dinheiro está sendo utilizado no MBL?",
+    a: "No grupo dos contribuidores, disponibilizaremos as prestações de contas do que estamos adquirindo com o dinheiro das mensalidades e doações.",
+  },
+  {
+    q: "Não tenho condições de contribuir com dinheiro, há alguma outra forma de ajudar?",
+    a: "Sim! Você pode entrar para nossos grupos de apoio. Temos reuniões mensais com os apoiadores e quem quiser colaborar com o seu conhecimento em áreas como redes sociais, imprensa, produção audiovisual, entre outros, será muito bem-vindo. Acesse participe.mbl.org.br e faça parte!",
+  },
+  {
+    q: "Como posso ter ainda mais proximidade com o MBL?",
+    a: "Nós temos a Academia MBL, um curso completo para quem quer aprender a participar de forma mais ativa do movimento. Todos os anos são abertas matrículas para novas turmas. Nossos coordenadores e principais líderes são professores e alunos do segundo ano da Academia. Acesse academia.mbl.org.br e saiba mais!",
+  },
+  {
+    q: "Sou aluno da Academia MBL, posso ser apoiador?",
+    a: "Lógico! Qualquer aluno da Academia, seja do primeiro ou do segundo ano, pode se tornar mensalista a qualquer momento.",
+  },
+  {
+    q: "Como faço para cancelar minha assinatura?",
+    a: "Basta efetuar o cancelamento seguindo as instruções da Stripe. A assinatura pode ser cancelada a qualquer momento. Se precisar de ajuda, estamos a disposição nos grupos de mensalistas e redes sociais.",
+  },
+  {
+    q: "Tenho mais dúvidas!",
+    a: "Entre em contato conosco nos nossos grupos de apoio (participe.mbl.org.br) ou redes sociais.",
   },
 ])
 </script>
