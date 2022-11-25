c7601c11980b2652c7cbdc3c4e9ee0630e9ac70b
Davi Moura <davisilvag4mer@gmail.com>
2022-11-24 21:39:43 -0300

    feat: plans as array

--- src/components/StripeSection.vue
+++ src/components/StripeSection.vue
@@ -1,3 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const plans = ref([
+  {
+    name: 'Mensalista I',
+    link: 'https://buy.stripe.com/8wMeXYaEydh8fwAdQQ',
+    price: 'R$ 20,00',
+  },
+  {
+    name: 'Mensalista II',
+    link: 'https://buy.stripe.com/00gg22h2Wgtkacg145',
+    price: 'R$ 50,00',
+  },
+  {
+    name: 'Mensalista III',
+    link: 'https://buy.stripe.com/fZedTUaEy6SK98c28a',
+    price: 'R$ 100,00',
+  },
+])
+</script>
+
 <template>
   <div class="xl:container mx-8 my-10" id="contribuir">
     <div class="mb-4">
@@ -9,50 +31,18 @@
       </b>
     </div>
     <div class="flex flex-col md:flex-row gap-8 justify-center">
-      <div>
+      <div v-for="plan of plans" :key="plan.name">
         <div class="card bg-base-200 shadow-lg">
           <!-- <figure><img src="https://placeimg.com/400/225/arch" alt="Mensalista I" /></figure> -->
           <div class="card-body">
-            <h2 class="card-title">Mensalista Nível I</h2>
+            <h2 class="card-title">{{ plan.name }}</h2>
             <p>
               ✅ Descontos nos nossos produtos<br />
               ✅ Acesso a grupo exclusivo de mensalistas<br />
               ✅ Reuniões mensais com coordenadores<br />
             </p>
             <div class="card-actions justify-center mt-4">
-              <a href="https://buy.stripe.com/test_dR69Dw5z33D01Py147" class="btn btn-primary">Assinar por R$ 20,00</a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div>
-        <div class="card bg-base-200 shadow-lg">
-          <!-- <figure><img src="https://placeimg.com/400/225/arch" alt="Mensalista" /></figure> -->
-          <div class="card-body">
-            <h2 class="card-title">Mensalista Nível II</h2>
-            <p>
-              ✅ Descontos nos nossos produtos<br />
-              ✅ Acesso a grupo exclusivo de mensalistas<br />
-              ✅ Reuniões mensais com coordenadores<br />
-            </p>
-            <div class="card-actions justify-center mt-4">
-              <a href="https://buy.stripe.com/test_8wM02Wd1v2yW8dWbIK" class="btn btn-primary">Assinar por R$ 50,00</a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div>
-        <div class="card bg-base-200 shadow-lg">
-          <!-- <figure><img src="https://placeimg.com/400/225/arch" alt="Mensalista" /></figure> -->
-          <div class="card-body">
-            <h2 class="card-title">Mensalista Nível III</h2>
-            <p>
-              ✅ Descontos nos nossos produtos<br />
-              ✅ Acesso a grupo exclusivo de mensalistas<br />
-              ✅ Reuniões mensais com coordenadores<br />
-            </p>
-            <div class="card-actions justify-center mt-4">
-              <a href="https://buy.stripe.com/test_fZecPI9Pj2yW2TCaEF" class="btn btn-primary">Assinar por R$ 100,00</a>
+              <a :href="plan.link" class="btn btn-primary">Assinar por {{ plan.price }}</a>
             </div>
           </div>
         </div>
