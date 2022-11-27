48b30d3a2d5754a6b7056190e5836b04bebf1354
Davi Moura <davisilvag4mer@gmail.com>
2022-11-27 16:01:40 -0300

    feat: basic auth

--- src/pages/mensalistas.vue
+++ src/pages/mensalistas.vue
@@ -29,17 +29,21 @@ interface Data {
 }
 
 const data = ref<Data>({ mensalistas: [], financeiro: { amount: 0 }, historico: [] })
-const loading = ref(true)
+const authenticated = ref(false)
+const passkey = ref('')
+const loading = ref(false)
 
 const mensalistasUrl = `https://workflow.davimoura.com.br/webhook/mblrj/mensalistas`
 
 async function loadData() {
-  const res = await axios.get(mensalistasUrl)
-  data.value = res.data
+  loading.value = true
+  try {
+    const res = await axios.get(mensalistasUrl, { params: { authorization: passkey.value } })
+    data.value = res.data
+    authenticated.value = true
+  } catch (err) {}
   loading.value = false
 }
-
-loadData()
 </script>
 
 <template>
@@ -55,6 +59,20 @@ loadData()
         <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
       </svg>
       <span class="sr-only">Loading...</span>
+    </div>
+  </div>
+  <div v-else-if="!authenticated">
+    <div class="flex flex-col md:flex-row gap-8 justify-center mb-20">
+      <div>
+        <div class="card bg-base-200 shadow-lg">
+          <div class="card-body">
+            <div class="card-actions justify-center mt-4">
+              <input v-model="passkey" type="password" class="input input-bordered w-full max-w-xs" placeholder="Senha" />
+              <button @click="loadData" class="btn btn-primary">Acessar</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
   <div class="container mx-auto pb-20" v-else>
