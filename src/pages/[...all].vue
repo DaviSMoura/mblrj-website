53b83aca74419481306cbd526ddf6ea7f5b14c0e
Davi Moura <davisilvag4mer@gmail.com>
2022-11-27 13:47:03 -0300

    feat: 404 page

---
+++ src/pages/[...all].vue
@@ -0,0 +1,11 @@
+<template>
+<div class="hero min-h-screen bg-base-200">
+  <div class="hero-content text-center">
+    <div class="max-w-md">
+      <h1 class="text-5xl font-bold">Erro 404</h1>
+      <p class="py-6">Você chegou a uma página que não existe!</p>
+      <button @click="$router.go(-1)" class="btn btn-primary">Voltar</button>
+    </div>
+  </div>
+</div>
+</template>
