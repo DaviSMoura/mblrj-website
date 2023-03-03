8efa33a5939b25c0ae75e8d16c2577e287256b96
Davi Moura <davisilvag4mer@gmail.com>
2023-03-02 23:29:20 -0300

    chore: add iconify

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
+import Icons from 'unplugin-icons/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    Pages()
+    Pages(),
+    Icons({})
   ],
   ssgOptions: {
     script: 'async',
